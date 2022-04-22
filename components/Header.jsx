03044aaf9a1188bf3f7273cb8ea643929c17cf93
Thomas Ehmann <ehmannth@gmail.com>
2022-04-22 08:51:22 +0200

    chore: change blog title

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <div className="block md:float-left">
           <Link href="/">
             <span className="text-4xl font-bold text-white cursor-pointer">
-              CMS Blog
+              Wels im April
             </span>
           </Link>
         </div>
